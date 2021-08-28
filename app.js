56b0378b618801fb14bfabff2a4a99dc1acd7492
Federico Barbieri <federicobarbieri54@gmail.com>
2021-08-28 08:58:34 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
-const welcome = document.querySelector('.welcome');
 
-welcome.style.color = 'blue';
+
+
+
+
+// CLASSES
+
+class Animal {
+  constructor(name, species, age, hobby, superpower){
+    this._name = name;
+    this._species = species;
+    this._age = age;
+    this._hobby = hobby;
+    this._superpower = superpower;
+  }
+  get name(){
+    return this._name;
+  }
+  get age(){
+    return this._age;
+  }
+  get hobby(){
+    return this._hobby;
+  }
+  get superpower(){
+    return this._superpower;
+  }
+}
